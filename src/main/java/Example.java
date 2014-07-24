6321ca9edaa19ff130a6b11d91474ea6d821be1f
dV <dVaffection@gmail.com>
2014-07-23 23:21:16 -0700

    Init

---
+++ src/main/java/Example.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello spring boot!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
